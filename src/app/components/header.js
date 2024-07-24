a441762426d4761ab7140d5c08b484c134aef0db
youssefrizk2 <166840867+youssefrizk2@users.noreply.github.com>
2024-07-24 18:05:16 +0300

    header added

---
+++ src/app/components/header.js
@@ -0,0 +1,34 @@
+import styles from './header.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.logodiv}>
+                <Link href="/"><Image className={styles.logo} src="/logo.png" alt="Logo" width={200} height={55}/></Link>
+            </div>
+
+            <div className={styles.linksdiv}>
+                <Link className={styles.nav} href="/">MEET THE TEAM</Link>
+                <Link className={styles.nav} href="/">CANDIDATES</Link>
+                <Link className={styles.nav} href="/">CLIENTS</Link>
+                <Link className={styles.nav} href="/">OTHER SERVICES</Link>
+            </div>
+
+            <div className={styles.actiondiv}>
+                <button className={styles.jobsearch}>JOB SEARCH</button>
+                <button className={styles.register}>REGISTER</button>
+                <button className={styles.enquire}>ENQUIRE</button>
+            </div>
+
+            <div className={styles.socialdiv}>
+                <Image src="/optimus_instagram.png" alt="Instagram" width={32} height={32}/>
+                <Image src="/optimus_linkedin.png" alt="LinkedIn" width={32} height={32}/>
+                <Image src="/optimus_facebook.png" alt="Facebook" width={32} height={32}/>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/icon.png" />
       </head>
       <body className={inter.className}>
+        <Header />
         {children}
       </body>
     </html>
