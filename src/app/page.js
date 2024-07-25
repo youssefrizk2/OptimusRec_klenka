923c3549ea0df3a1f9d4232cccb40c7e216dc591
youssefrizk2 <166840867+youssefrizk2@users.noreply.github.com>
2024-07-25 22:12:04 +0300

    many adds

--- src/app/page.js
+++ src/app/page.js
@@ -33,6 +33,25 @@ export default function Home() {
         <input className={styles.dropdown} type="text" placeholder="Keyword" />
         <button className={styles.searchbutton}>Search</button>
       </div>
+      <div className={styles.aboutus}>
+        <div className={styles.aboutlogo}>
+          <Image  src="/icon.png" alt="logo" width={35} height={35} className={styles.aboutimg}/>
+          ABOUT US
+        </div>
+        <p className={styles.abouttext}>
+        Optimus has quickly established itself as a prominent force within financial <br/>
+        recruitment in Jersey. In the past 15 years we've forged strong links with clients <br/>
+        and candidates and are often regarded by both as the recruitment company of <br/>
+        choice for skilled, ambitious individuals and one of the first places clients turn to <br/>
+        when looking for talent. <br/>
+        Within the team at Optimus you're likely to find someone who has performed <br/>
+        the very role you're looking for. Our expertise and practical experience spans <br/>
+        the majority of the core financial services areas including Fiduciary, Funds, <br/>
+        Wealth Management, HR, IT, Compliance and Marketing in a range of <br/>
+        organisations from blue chip global banks to independent trust companies
+        </p>
+      </div>
+
 
     </div>
   );
