5ec6920ba5aba5fc202749c8cab44c5d0bdaeb9c
youssefrizk2 <166840867+youssefrizk2@users.noreply.github.com>
2024-08-05 18:18:08 +0300

    added outplacement and news and adjusted social media buttons

--- src/app/meetteam/page.js
+++ src/app/meetteam/page.js
@@ -3,7 +3,11 @@ import styles from "./meet.module.css";
 const Meet = () => {
     return (
         <div className={styles.container}>
-
+            <div className={styles.image}>
+                <div className={styles.imagetext}>
+                Meet the Optimus Team...
+                </div>
+            </div>
         </div>
     );
 }
